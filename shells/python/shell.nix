77c14206b9994f29f71379e30fcecc56b5e2b4c7
ryanlyn <mailboxryanlin@icloud.com>
2023-09-12 09:09:25 +1000

    add sample python shell

---
+++ shells/python/shell.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import <nixpkgs> {} }:
+with pkgs;
+mkShell rec {
+  name = "python-env";
+  LD_LIBRARY_PATH = lib.makeLibraryPath [ 
+    gcc-unwrapped 
+    zlib 
+    libglvnd 
+    glib 
+  ];
+  buildInputs = [ 
+    python310 
+    python310Packages.pip 
+    git 
+  ];
+}
